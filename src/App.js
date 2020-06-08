ff9408ef4d9a6f11eaf4d6cdb89e31dad49a91fe
Ante Hellgren <antehellgren@gmail.com>
2020-06-08 22:16:19 +0200

    imported navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 
+import HomeScreen from './screens/HomeScreen';
 import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <BrowserRouter>
       <NavigationBar/>
+      <Route path='/' exact component={HomeScreen}/>
     </BrowserRouter>
     
   );
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function HomeScreen() {
   return (
     <div>
-      
+      <h2>Home screen</h2>
+      <p> News and pictures</p>
     </div>
   )
 }
