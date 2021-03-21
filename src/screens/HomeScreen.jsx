b6fd116e022c40bd2d5a5ed3a8b5a5b03866ccb4
Ante Hellgren <antehellgren@gmail.com>
2021-03-21 08:38:57 +0100

    removed tokbox test from master

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -7,8 +7,7 @@ export default function HomeScreen() {
       <div className="container-carousel">
         <h2>Senast inkommet</h2>
         {/* <Carousel/> */}
-        <div id="otEmbedContainer" style={{width:'800px', height:'640px'}}>
-          </div> 
+        
           
       </div>
     </div>
